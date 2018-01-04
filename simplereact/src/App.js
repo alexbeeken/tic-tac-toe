4ed9239539affe45292802a2ee319d436940498f
Alex Beeken <aeb242@gmail.com>
2018-01-03 22:58:47 -0800

    draw board in react app

--- simplereact/src/App.js
+++ simplereact/src/App.js
@@ -6,20 +6,21 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      message: 'Hello World!'
+      PLAYER_ONE_SYMBOL: 'X',
+      PLAYER_TWO_SYMBOL: 'O',
+      currentTurn: 'X',
+      board: [
+        "","","","","","","","",""
+      ]
     }
   }
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 onClick={() => this.setState({message: this.state.message + '!'})} className="App-title">{this.state.message}</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='board'>
+        {this.state.board.map((cell) => {
+          return <div className='square'>{cell}</div>
+        })}
       </div>
     );
   }
